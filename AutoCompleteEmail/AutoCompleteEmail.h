b26c2618a137232234d9c46c6998ffbde3fdeff3
mihcristina <contato@michellicristina.com.br>
2022-02-21 14:31:58 -0300

    first commit

---
+++ AutoCompleteEmail/AutoCompleteEmail.h
@@ -0,0 +1,18 @@
+//
+//  AutoCompleteEmail.h
+//  AutoCompleteEmail
+//
+//  Created by Michelli Cristina de Paulo Lima on 21/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoCompleteEmail.
+FOUNDATION_EXPORT double AutoCompleteEmailVersionNumber;
+
+//! Project version string for AutoCompleteEmail.
+FOUNDATION_EXPORT const unsigned char AutoCompleteEmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoCompleteEmail/PublicHeader.h>
+
+
